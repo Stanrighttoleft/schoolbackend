5668aa716ad1d27a4c4453d7ae8c1096fef48543
stan <drivesafe24@myyahoo.com>
2025-09-30 16:37:57 +0800

    lesson 10 and the backend modify for image

---
+++ Lesson-10/index_s01.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Lobster&family=Noto+Sans+TC:wght@100..900&family=Noto+Serif+TC:wght@200..900&display=swap');
+    @import url(http://fonts.googleapis.com/earlyaccess/cwtexhei.css);
+    @import url(http://fonts.googleapis.com/earlyaccess/cwtexfangsong.css);
+    @import url(http://fonts.googleapis.com/earlyaccess/cwtexkai.css);
+    @import url(http://fonts.googleapis.com/earlyaccess/cwtexming.css);
+    @import url(http://fonts.googleapis.com/earlyaccess/cwtexyen.css);
+
+    </style>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.2.1/css/all.css">
+•
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+     <link rel="stylesheet" href="./website_s01.css">
+    <title>店商藥妝</title>
+</head>
+<body>
+    <section id="header">
+        <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="images/logo.jpg" class="img-fluid rounded-circle" alt="店傷藥妝"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                
+                <li class="nav-item">
+                <a class="nav-link" href="#">會員註冊</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">會員登入</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">會員中心</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">最新活動</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">查訂單</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">折價券</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">購物車</a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    企業專區
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">認識企業</a></li>
+                    <li><a class="dropdown-item" href="#">全台門市</a></li>
+                    <li><a class="dropdown-item" href="#">供應商報</a></li>
+                    <li><a class="dropdown-item" href="#">加盟專區</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">投資人專區</a></li>
+                </ul>
+               
+            </ul>
+            
+            </div>
+        </div>
+        </nav>        
+    </section>
+    <section id="content"></section>
+    <section id="scontent"></section>
+    <section id="footer"></section>
+
+    
+    
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+</html>
